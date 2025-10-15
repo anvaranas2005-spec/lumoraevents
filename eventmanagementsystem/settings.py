01a35ca785e3c9145424ee156a13279185d915b9
23bca002-hp <23bca002@stc.ac.in>
2025-10-15 11:46:44 +0530

    yes

--- eventmanagementsystem/settings.py
+++ eventmanagementsystem/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-m0&gahw!mee^+%#dil)4vczjs6)-=b$3hj+yy4iq@ge-0ig0(5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com']
+ALLOWED_HOSTS = ['lumoraevents.onrender.com']
 
 
 # Application definition
